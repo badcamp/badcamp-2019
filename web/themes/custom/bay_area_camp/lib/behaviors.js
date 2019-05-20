90ef69f4fe3c44608ab0e444495181a87924da6a
M Miller <miller.2676@gmail.com>
2019-05-20 11:52:02 -0700

    fix article teaser style while logged in as editor

--- web/themes/custom/bay_area_camp/lib/behaviors.js
+++ web/themes/custom/bay_area_camp/lib/behaviors.js
@@ -1,20 +1,35 @@
 (function(Drupal, $) {
-
   Drupal.behaviors.foundation = {
-    attach:function(context) {
+    attach: function(context) {
       $(context).foundation();
-    }
+    },
   };
 
   Drupal.behaviors.badAreaTabs = {
-    attach: function (context, settings) {
-      $('.tabs-trigger').click(function(e){
+    attach: function(context, settings) {
+      $('.tabs-trigger').click(function(e) {
         e.preventDefault();
-        $('.tabs-list').parent().toggleClass('open');
+        $('.tabs-list')
+          .parent()
+          .toggleClass('open');
         $('.fa', this).toggleClass('fa-cog');
         $('.fa', this).toggleClass('fa-close');
       });
-    }
+    },
   };
 
-}(Drupal, jQuery));
+  // Make article teasers look correct while logged in as a site editor.
+  Drupal.behaviors.articleTeaser = {
+    attach: function(context, settings) {
+      $(context)
+        .find('.article__listing')
+        .each(function() {
+          var contextEdit = $('.contextual-region.article__display--teaser');
+          var row = $('.views-row');
+          if (contextEdit.length) {
+            row.css('padding', '0');
+          }
+        });
+    },
+  };
+})(Drupal, jQuery);
