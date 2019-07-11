457f40e9038afb4fbb2c259cc9d343a2d46fc3bc
Sean Dietrich <sean@kanopistudios.com>
2019-07-11 11:06:37 -0700

    Simplified controller

--- web/modules/custom/badcamp/src/Controller/SessionSubmissionController.php
+++ web/modules/custom/badcamp/src/Controller/SessionSubmissionController.php
@@ -3,8 +3,6 @@
 namespace Drupal\badcamp\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Session\AccountInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
@@ -15,37 +13,11 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 class SessionSubmissionController extends ControllerBase{
 
   /**
-   * The Account Service.
-   *
-   * @var \Drupal\Core\Session\AccountInterface $account
-   */
-  protected $account;
-
-  /**
-   * SessionSubmissionController constructor.
-   *
-   * @param \Drupal\Core\Session\AccountInterface
-   *   The Account Service.
-   */
-  public function __construct(AccountInterface $account) {
-    $this->account = $account;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('current_user')
-    );
-  }
-
-  /**
    * Checks if user is logged in.
    */
   public function checkUser() {
     $url = '';
-    if ($this->currentUser->isAuthenticated()) {
+    if ($this->currentUser()->isAuthenticated()) {
       $url = '/node/add/session';
     }
     else {
