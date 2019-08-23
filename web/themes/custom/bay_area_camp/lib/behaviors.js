de6ece3d1c58f615c7ce40ca71084c1bd051a1c0
M Miller <miller.2676@gmail.com>
2019-08-23 10:57:56 -0700

    rework article teaser

--- web/themes/custom/bay_area_camp/lib/behaviors.js
+++ web/themes/custom/bay_area_camp/lib/behaviors.js
@@ -4,19 +4,4 @@
       $(context).foundation();
     },
   };
-
-  // Make article teasers look correct while logged in as a site editor.
-  Drupal.behaviors.articleTeaser = {
-    attach: function(context, settings) {
-      $(context)
-        .find('.article__listing')
-        .each(function() {
-          var contextEdit = $('.contextual-region.article__display--teaser');
-          var row = $('.views-row');
-          if (contextEdit.length) {
-            row.css('padding', '0');
-          }
-        });
-    },
-  };
 })(Drupal, jQuery);
